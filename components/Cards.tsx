6fdcb98de81baaa13c221c0652abe9689f9daf09
pricillia.myjoulebox <pricillia.edou-edou@myjoulebox.com>
2025-03-02 03:28:53 +0100

    add components card and PokemonCard (listing)

---
+++ components/Cards.tsx
@@ -0,0 +1,19 @@
+import { Shadows } from "@/constants/Shadows";
+import { useThemeColors } from "@/hooks/useThemeColors";
+import { View, ViewStyle, type ViewProps } from "react-native";
+
+type Props = ViewProps;
+export function Card({ style, ...rest }: Props) {
+  const colors = useThemeColors();
+  return (
+    <View
+      style={[style, styles, { backgroundColor: colors.grayWhite }]}
+      {...rest}
+    />
+  );
+}
+
+const styles = {
+  borderRadius: 8,
+  ...Shadows.dp2,
+} satisfies ViewStyle;
